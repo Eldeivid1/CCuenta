8ad1347f0eb6a6ea28d628cc088f491f0216dd68
Eldeivid1 <david.edufis@gmail.com>
2023-03-10 19:24:39 +0100

    Ultima version

--- CCuenta.java
+++ CCuenta.java
@@ -1,40 +1,102 @@
+/** @author david*/
+/** @version 3-2.0*/
+
+
+
 public class CCuenta {
 
+	private float cantidad;				/** @param cantidad */
+    private String nombre;				/** @param nombre */ 
+    private String cuenta; 				/** @param cuenta */ 
+    private double saldo; 				/** @param saldo */
+    private double tipoInteres;			 /** @param tipo de interés*/
 
-    private String nombre;
-    private String cuenta;
-    private double saldo;
-    private double tipoInterés;
-
-    public CCuenta()
+    public CCuenta() 					/** metodo*/
     {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
+    
+    public CCuenta(String nom, String cue, double sal, double tipo) /** metodo*/
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+        setNombre(nom); 					/** @param nombre */
+        setCuenta(cue);					 	/** @param cuenta */
+        setSaldo(sal); 						/** @param saldo */
     }
 
-    public double estado()
+    public double estado() 
     {
-        return saldo;
+        return getSaldo();					/**@return devuelve saldo*/
     }
 
-    public void ingresar(double cantidad) throws Exception
+    public void ingresar(double cantidad) throws Exception /** metodo*/
     {
-        if (cantidad<0)
+        if (cantidad<0) 
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        setSaldo(getSaldo() + cantidad);		/**@return devuelve saldo nuevo*/
     }
 
-    public void retirar(double cantidad) throws Exception
+    public void retirar(double cantidad) throws Exception/** metodo*/
     {
-        if (cantidad <= 0)
+        if (cantidad <= 0)				/**metodo*/
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        setSaldo(getSaldo() - cantidad);
     }
+
+	/**
+	 * @return the nombre
+	 */
+	private String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the cuenta
+	 */
+	private String getCuenta() {
+		return cuenta;
+	}
+
+	/**
+	 * @param cuenta the cuenta to set
+	 */
+	private void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	/**
+	 * @return the saldo
+	 */
+	private double getSaldo() {
+		return saldo;
+	}
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	/**
+	 * @return the tipoInteres
+	 */
+	private double getTipoInteres() {
+		return tipoInteres;
+	}
+
+	/**
+	 * @param tipoInteres the tipoInteres to set
+	 */
+	private void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,24 @@
-public class Main {
+/** @author david*/
+/** @version 3-2.0*/
 
-    public static void main(String[] args) {
+
+public class Main {																			/** Clase Main*/
+	/** @version 3-2.0*/
+
+    public static void main(String[] args) {												/** Método Main*/
         CCuenta miCuenta;
-        double saldoActual;
+        double saldoActual;																	/** @param saldo*/
 
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es"+ saldoActual );								/** @return saldo actual*/
 
-        try {
+        try {																				/** Método retirar*/
             miCuenta.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
-        try {
+        try {																				/** Método ingresar*/
             System.out.println("Ingreso en cuenta");
             miCuenta.ingresar(695);
         } catch (Exception e) {
